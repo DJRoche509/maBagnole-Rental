5fab0533a25a7ba5c61c815b06dc3e4de2276c11
DJRoche509 <davjroche@gmail.com>
2023-05-23 21:26:12 -0500

    Implement carReservation() with date/time

--- public/js/main.js
+++ public/js/main.js
@@ -1,1 +1,23 @@
 
+const reserve = document.getElementById("reserve-form");
+
+
+const carReservation = event => {
+    event.preventDefault();
+
+    // Get the selected pick-up date and time
+    const pickupDate = document.getElementById("pickup-date").value;
+    const pickupTime = document.getElementById("pickup-time").value;
+
+    // Get the selected return date and time
+    const returnDate = document.getElementById("return-date").value;
+    const returnTime = document.getElementById("return-time").value;
+
+    // Log out the selected date and time values
+    console.log(`Pick-up date: ${pickupDate}`);
+    console.log(`Pick-up time: ${pickupTime}`);
+    console.log(`Return date: ${returnDate}`);
+    console.log(`Return time: ${returnTime}`);
+}
+
+reserve.addEventListener("submit", carReservation);
